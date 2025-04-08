df14c7d5e9cab0043ed1fe17e6061f9c194c3d63
Bartosz Nowak <bartekn337@gmail.com>
2025-04-08 16:10:06 +0200

    full trace proof

--- stwo_cairo_prover/crates/adapted_prover/src/main.rs
+++ stwo_cairo_prover/crates/adapted_prover/src/main.rs
@@ -1,92 +1,18 @@
-use std::path::PathBuf;
 use std::process::ExitCode;
 
 use cairo_air::verifier::{verify_cairo, CairoVerificationError};
-use cairo_air::PreProcessedTraceVariant;
-use clap::Parser;
-use serde::Serialize;
-use stwo_cairo_adapter::vm_import::{adapt_vm_output, VmImportError};
+use stwo_cairo_adapter::builtins::BuiltinSegments;
+use stwo_cairo_adapter::memory::{EncodedMemoryValueId, Memory, MemoryConfig};
+use stwo_cairo_adapter::opcodes::{CasmStatesByOpcode, StateTransitions};
+use stwo_cairo_adapter::vm_import::VmImportError;
 use stwo_cairo_adapter::ProverInput;
-use stwo_cairo_prover::prover::{
-    default_prod_prover_parameters, prove_cairo, ChannelHash, ProverParameters,
-};
-use stwo_cairo_serialize::CairoSerialize;
+use stwo_cairo_prover::prover::{default_prod_prover_parameters, prove_cairo, ProverParameters};
+use stwo_cairo_prover::witness::prelude::{CasmState, M31};
 use stwo_cairo_utils::binary_utils::run_binary;
-use stwo_cairo_utils::file_utils::{create_file, read_to_string, IoErrorWithPath};
-use stwo_prover::core::backend::simd::SimdBackend;
-use stwo_prover::core::backend::BackendForChannel;
-use stwo_prover::core::channel::MerkleChannel;
-use stwo_prover::core::pcs::PcsConfig;
+use stwo_cairo_utils::file_utils::IoErrorWithPath;
 use stwo_prover::core::prover::ProvingError;
 use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleChannel;
-use stwo_prover::core::vcs::ops::MerkleHasher;
-use stwo_prover::core::vcs::poseidon252_merkle::Poseidon252MerkleChannel;
 use thiserror::Error;
-use tracing::{span, Level};
-
-/// Command line arguments for adapted_stwo.
-/// Example command line:
-///     ```
-///     cargo run -r --bin adapted_stwo -- --pub_json absolute/path/to/pub.json
-///     --priv_json absolute/path/to/priv.json --proof_path path/to/proof
-///     ```
-///
-/// For optimal performance compile with:
-///     ```
-///   RUSTFLAGS="-C target-cpu=native -C opt-level=3"
-///     ```
-///
-/// Standard library features is off by default, to enable it, further improving performance, use:
-///     ```
-///     --features=std
-///     ```
-#[derive(Parser, Debug)]
-struct Args {
-    #[structopt(long = "pub_json")]
-    pub_json: PathBuf,
-    #[structopt(long = "priv_json")]
-    priv_json: PathBuf,
-    /// The path to the JSON file containing the prover parameters (optional).
-    /// The expected file format is:
-    ///     {
-    ///         "channel_hash":"blake2s",
-    ///         "pcs_config": {
-    ///             "pow_bits": 26,
-    ///             "fri_config": {
-    ///                 "log_last_layer_degree_bound": 0,
-    ///                 "log_blowup_factor": 1,
-    ///                 "n_queries": 70
-    ///             }
-    ///         },
-    ///         "preprocessed_trace": "canonical_without_pedersen"
-    ///     }
-    ///
-    /// Default parameters are chosen to ensure 96 bits of security.
-    #[structopt(long = "params_json")]
-    params_json: Option<PathBuf>,
-    /// The output file path for the proof.
-    #[structopt(long = "proof_path")]
-    proof_path: PathBuf,
-    /// The format of the proof output.
-    /// - json: Standard JSON format (default)
-    /// - cairo_serde: Array of field elements serialized as hex strings, ex. `["0x1", "0x2"]`
-    #[arg(long, value_enum, default_value_t = ProofFormat::Json)]
-    proof_format: ProofFormat,
-    #[structopt(long = "track_relations")]
-    track_relations: bool,
-    /// Verify the generated proof.
-    #[structopt(long = "verify")]
-    verify: bool,
-}
-
-#[derive(Debug, Clone, clap::ValueEnum)]
-enum ProofFormat {
-    /// Standard JSON format.
-    Json,
-    /// Array of field elements serialized as hex strings.
-    /// Compatible with `scarb execute`
-    CairoSerde,
-}
 
 #[derive(Debug, Error)]
 enum Error {
@@ -110,84 +36,120 @@ fn main() -> ExitCode {
     run_binary(run, "adapted_stwo")
 }
 
-fn run(args: impl Iterator<Item = String>) -> Result<(), Error> {
-    let _span = span!(Level::INFO, "run").entered();
-    let args = Args::try_parse_from(args)?;
-
-    let vm_output: ProverInput =
-        adapt_vm_output(args.pub_json.as_path(), args.priv_json.as_path())?;
-
-    log::info!(
-        "Casm states by opcode:\n{}",
-        vm_output.state_transitions.casm_states_by_opcode
-    );
+fn run(_args: impl Iterator<Item = String>) -> Result<(), Error> {
+    let prover_input: ProverInput = ProverInput {
+        state_transitions: StateTransitions {
+            initial_state: CasmState {
+                pc: M31(1),
+                ap: M31(12),
+                fp: M31(12),
+            },
+            final_state: CasmState {
+                pc: M31(9),
+                ap: M31(17),
+                fp: M31(12),
+            },
+            casm_states_by_opcode: CasmStatesByOpcode {
+                add_opcode_small: vec![CasmState {
+                    pc: M31(8),
+                    ap: M31(16),
+                    fp: M31(12),
+                }],
+                assert_eq_opcode_imm: vec![
+                    CasmState {
+                        pc: M31(1),
+                        ap: M31(12),
+                        fp: M31(12),
+                    },
+                    CasmState {
+                        pc: M31(3),
+                        ap: M31(13),
+                        fp: M31(12),
+                    },
+                    CasmState {
+                        pc: M31(5),
+                        ap: M31(14),
+                        fp: M31(12),
+                    },
+                ],
+                mul_opcode_small: vec![CasmState {
+                    pc: M31(7),
+                    ap: M31(15),
+                    fp: M31(12),
+                }],
+                ..Default::default()
+            },
+        },
+        instruction_by_pc: [
+            (M31(5), 5189976364521848832),
+            (M31(8), 5201798304953565184),
+            (M31(7), 5210805504208437248),
+            (M31(1), 5189976364521848832),
+            (M31(3), 5189976364521848832),
+        ]
+        .into_iter()
+        .collect(),
+        memory: Memory {
+            config: MemoryConfig {
+                small_max: 4722366482869645213695,
+            },
+            address_to_id: vec![
+                EncodedMemoryValueId(1073741823),
+                EncodedMemoryValueId(0),
+                EncodedMemoryValueId(1),
+                EncodedMemoryValueId(0),
+                EncodedMemoryValueId(2),
+                EncodedMemoryValueId(0),
+                EncodedMemoryValueId(3),
+                EncodedMemoryValueId(4),
+                EncodedMemoryValueId(5),
+                EncodedMemoryValueId(6),
+                EncodedMemoryValueId(7),
+                EncodedMemoryValueId(7),
+                EncodedMemoryValueId(1),
+                EncodedMemoryValueId(2),
+                EncodedMemoryValueId(3),
+                EncodedMemoryValueId(8),
+                EncodedMemoryValueId(9),
+            ],
+            inst_cache: [
+                (1, 5189976364521848832),
+                (5, 5189976364521848832),
+                (7, 5210805504208437248),
+                (3, 5189976364521848832),
+                (8, 5201798304953565184),
+            ]
+            .into_iter()
+            .collect(),
+            f252_values: vec![],
+            small_values: vec![
+                5189976364521848832,
+                1,
+                2,
+                3,
+                5210805504208437248,
+                5201798304953565184,
+                2345108766317314046,
+                17,
+                6,
+                7,
+            ],
+        },
+        public_memory_addresses: vec![],
+        builtins_segments: BuiltinSegments::default(),
+    };
 
     let ProverParameters {
-        channel_hash,
         pcs_config,
         preprocessed_trace,
-    } = match args.params_json {
-        Some(path) => serde_json::from_str(&read_to_string(&path)?)?,
-        None => default_prod_prover_parameters(),
-    };
+        ..
+    } = default_prod_prover_parameters();
 
-    let run_inner_fn = match channel_hash {
-        ChannelHash::Blake2s => run_inner::<Blake2sMerkleChannel>,
-        ChannelHash::Poseidon252 => run_inner::<Poseidon252MerkleChannel>,
-    };
+    let proof = prove_cairo::<Blake2sMerkleChannel>(prover_input, pcs_config, preprocessed_trace)?;
+    log::info!("Proof proven successfully");
 
-    run_inner_fn(
-        vm_output,
-        pcs_config,
-        preprocessed_trace,
-        args.verify,
-        args.proof_path,
-        args.proof_format,
-    )?;
-
-    Ok(())
-}
-
-/// Generates proof given the Cairo VM output and prover config/parameters.
-/// Serializes the proof as JSON and write to the output path.
-/// Verifies the proof in case the respective flag is set.
-fn run_inner<MC: MerkleChannel>(
-    vm_output: ProverInput,
-    pcs_config: PcsConfig,
-    preprocessed_trace: PreProcessedTraceVariant,
-    verify: bool,
-    proof_path: PathBuf,
-    proof_format: ProofFormat,
-) -> Result<(), Error>
-where
-    SimdBackend: BackendForChannel<MC>,
-    MC::H: Serialize,
-    <MC::H as MerkleHasher>::Hash: CairoSerialize,
-{
-    let proof = prove_cairo::<MC>(vm_output, pcs_config, preprocessed_trace)?;
-    let proof_file = create_file(&proof_path)?;
-
-    match proof_format {
-        ProofFormat::Json => {
-            serde_json::to_writer_pretty(proof_file, &proof)?;
-        }
-        ProofFormat::CairoSerde => {
-            let mut serialized: Vec<starknet_ff::FieldElement> = Vec::new();
-            CairoSerialize::serialize(&proof, &mut serialized);
-
-            let hex_strings: Vec<String> = serialized
-                .into_iter()
-                .map(|felt| format!("0x{:x}", felt))
-                .collect();
-
-            serde_json::to_writer_pretty(proof_file, &hex_strings)?;
-        }
-    }
-
-    if verify {
-        verify_cairo::<MC>(proof, pcs_config, preprocessed_trace)?;
-        log::info!("Proof verified successfully");
-    }
+    verify_cairo::<Blake2sMerkleChannel>(proof, pcs_config, preprocessed_trace)?;
+    log::info!("Proof verified successfully");
 
     Ok(())
 }
