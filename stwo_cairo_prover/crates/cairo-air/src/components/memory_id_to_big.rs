26362e67db17c97ec176a74858b171e84bbced12
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-09-18 10:10:43 +0200

    prover: filter out unused memory cells in id_to_big

--- stwo_cairo_prover/crates/cairo-air/src/components/memory_id_to_big.rs
+++ stwo_cairo_prover/crates/cairo-air/src/components/memory_id_to_big.rs
@@ -2,10 +2,8 @@ use itertools::{chain, Itertools};
 use num_traits::One;
 use serde::{Deserialize, Serialize};
 use stwo::core::channel::Channel;
-use stwo::core::fields::m31::M31;
 use stwo::core::fields::qm31::{SecureField, SECURE_EXTENSION_DEGREE};
 use stwo::core::pcs::TreeVec;
-use stwo_cairo_adapter::memory::LARGE_MEMORY_VALUE_ID_BASE;
 use stwo_cairo_common::memory::{N_M31_IN_FELT252, N_M31_IN_SMALL_FELT252};
 use stwo_cairo_serialize::{CairoDeserialize, CairoSerialize};
 use stwo_constraint_framework::{
@@ -13,14 +11,13 @@ use stwo_constraint_framework::{
 };
 
 use super::prelude::RelationUse;
-use crate::preprocessed::{PreProcessedColumn, Seq};
 use crate::relations;
 
 // TODO(AlonH): Make memory size configurable.
 pub const MEMORY_ID_SIZE: usize = 1;
 pub const N_MULTIPLICITY_COLUMNS: usize = 1;
-pub const BIG_N_COLUMNS: usize = N_M31_IN_FELT252 + N_MULTIPLICITY_COLUMNS;
-pub const SMALL_N_COLUMNS: usize = N_M31_IN_SMALL_FELT252 + N_MULTIPLICITY_COLUMNS;
+pub const BIG_N_COLUMNS: usize = N_M31_IN_FELT252 + MEMORY_ID_SIZE + N_MULTIPLICITY_COLUMNS;
+pub const SMALL_N_COLUMNS: usize = N_M31_IN_SMALL_FELT252 + MEMORY_ID_SIZE + N_MULTIPLICITY_COLUMNS;
 
 pub type BigComponent = FrameworkComponent<BigEval>;
 pub type SmallComponent = FrameworkComponent<SmallEval>;
@@ -141,8 +138,8 @@ impl FrameworkEval for BigEval {
     }
 
     fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
-        let seq = eval.get_preprocessed_column(Seq::new(self.log_size()).id());
         let value: [E::F; N_M31_IN_FELT252] = std::array::from_fn(|_| eval.next_trace_mask());
+        let id = eval.next_trace_mask();
         let multiplicity = eval.next_trace_mask();
 
         // Range check limbs.
@@ -196,9 +193,6 @@ impl FrameworkEval for BigEval {
         }
 
         // Yield the value.
-        let id = seq
-            + E::F::from(M31::from(LARGE_MEMORY_VALUE_ID_BASE))
-            + E::F::from(M31::from(self.offset));
         eval.add_to_relation(RelationEntry::new(
             &self.lookup_elements,
             E::EF::from(-multiplicity),
@@ -289,8 +283,8 @@ impl FrameworkEval for SmallEval {
     }
 
     fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
-        let seq = eval.get_preprocessed_column(Seq::new(self.log_size()).id());
         let value: [E::F; N_M31_IN_SMALL_FELT252] = std::array::from_fn(|_| eval.next_trace_mask());
+        let id = eval.next_trace_mask();
         let multiplicity = eval.next_trace_mask();
 
         // Range check limbs.
@@ -324,7 +318,6 @@ impl FrameworkEval for SmallEval {
         }
 
         // Yield the value.
-        let id = seq;
         eval.add_to_relation(RelationEntry::new(
             &self.lookup_elements,
             E::EF::from(-multiplicity),
