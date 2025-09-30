760f582458a253750a9caa5a6d08c9e45d1e621f
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-09-30 14:29:37 +0200

    rebase: fixed rebase issues

--- stwo_cairo_prover/crates/cairo-air/src/components/memory_address_to_id.rs
+++ stwo_cairo_prover/crates/cairo-air/src/components/memory_address_to_id.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 use stwo::core::channel::Channel;
 use stwo::core::fields::qm31::{SecureField, SECURE_EXTENSION_DEGREE};
 use stwo::core::pcs::TreeVec;
-use stwo_cairo_common::preprocessed_columns::preprocessed_trace::{PreProcessedColumn, Seq};
 use stwo_cairo_serialize::{CairoDeserialize, CairoSerialize};
 use stwo_constraint_framework::{EvalAtRow, FrameworkComponent, FrameworkEval, RelationEntry};
 
